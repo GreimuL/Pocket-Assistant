af7029e1d4e95090ec4415844b37c75c2cacebb5
greimul <yoseib1998@naver.com>
2020-07-17 23:46:52 +0900

    Add GestureListener

--- concept/crawlertest/app/src/main/java/com/greimul/crawlertest/OverlayService.kt
+++ concept/crawlertest/app/src/main/java/com/greimul/crawlertest/OverlayService.kt
@@ -11,10 +11,13 @@ import android.os.IBinder
 import android.util.Log
 import android.view.*
 import android.widget.TextView
+import androidx.core.view.GestureDetectorCompat
 import androidx.core.view.MotionEventCompat
 import kotlinx.android.synthetic.main.view_test.view.*
 import kotlinx.android.synthetic.main.view_test2.view.*
+import kotlinx.android.synthetic.main.view_test3.view.*
 import kotlinx.coroutines.*
+import java.lang.Math.abs
 import kotlin.random.Random
 
 class OverlayService: Service() {
@@ -22,6 +25,13 @@ class OverlayService: Service() {
     lateinit var animTest:AnimationDrawable
     lateinit var view:View
     lateinit var view2:View
+    lateinit var view3:View
+    lateinit var mDetector:GestureDetectorCompat
+    lateinit var paramsTest:WindowManager.LayoutParams
+    lateinit var paramsTest2:WindowManager.LayoutParams
+    lateinit var paramsTest3:WindowManager.LayoutParams
+    lateinit var windowManager:WindowManager
+    val strArr = arrayOf("안녕하세요","테스트에요","초밥이먹고싶어요","초밥주세요","배고파요")
     private val binder = LocalBind()
     override fun onBind(intent: Intent?): IBinder? {
         return binder
@@ -33,17 +43,23 @@ class OverlayService: Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
-        val windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
         view = LayoutInflater.from(this).inflate(R.layout.view_test,null)
         view2 = LayoutInflater.from(this).inflate(R.layout.view_test2,null)
+        view3 = LayoutInflater.from(this).inflate(R.layout.view_test3,null)
         view.imageview_main.apply{
             setBackgroundResource(R.drawable.testhide)
             //animTest = background as AnimationDrawable
         }
+        view3.button.setOnClickListener {
+            view3.button.text = "3Sec"
+            view3.button2.text = "5Sec"
+            view3.button3.text = "7Sec"
+        }
         //animTest.start()
 
-        val strArr = arrayOf("안녕하세요","테스트에요","초밥이먹고싶어요","초밥주세요","배고파요")
-        var paramsTest = WindowManager.LayoutParams(
+
+        paramsTest = WindowManager.LayoutParams(
             WindowManager.LayoutParams.WRAP_CONTENT,
             WindowManager.LayoutParams.WRAP_CONTENT,
             WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
@@ -53,7 +69,7 @@ class OverlayService: Service() {
             PixelFormat.TRANSLUCENT).apply{
             gravity = Gravity.CENTER or Gravity.RIGHT
         }
-        var paramsTest2 = WindowManager.LayoutParams(
+        paramsTest2 = WindowManager.LayoutParams(
             WindowManager.LayoutParams.WRAP_CONTENT,
             WindowManager.LayoutParams.WRAP_CONTENT,
             WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
@@ -63,36 +79,30 @@ class OverlayService: Service() {
             PixelFormat.TRANSLUCENT).apply{
             gravity = Gravity.CENTER or Gravity.RIGHT
         }
+
+        paramsTest3 = WindowManager.LayoutParams(
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or
+                    WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or
+                    WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+            PixelFormat.TRANSLUCENT).apply{
+            gravity = Gravity.CENTER or Gravity.RIGHT
+        }
+
         paramsTest2.y = paramsTest.y -200
+        paramsTest3.x = paramsTest.x + 200
         var oriX:Float = 0F
         var oriY:Float = 0F
         var imgPreX:Int = 0
         var imgPreY:Int = 0
         var txtPreY = 0
-        view.imageview_main.setOnTouchListener { v, event ->
+
+        mDetector = GestureDetectorCompat(this,GestureListener())
+
+        view.imageview_main.setOnTouchListener{v,event->
             when(event.action){
-                MotionEvent.ACTION_DOWN->{
-                    oriY = event.rawY
-                    imgPreY = paramsTest.y
-                    txtPreY = paramsTest2.y
-                    view2.textview_overlay.text = strArr[Random.nextInt(5)]
-                }
-                MotionEvent.ACTION_MOVE->{
-                    var curY = (event.rawY -oriY).toInt()
-                    Log.d("aasff","${event.rawY}")
-                    paramsTest.y = imgPreY + curY
-                    paramsTest2.y = txtPreY+ curY
-
-                    view2.textview_overlay.text = ""
-                    view.imageview_main.apply{
-                        setBackgroundResource(R.drawable.anim_test)
-                        animTest = background as AnimationDrawable
-                    }
-                    animTest.start()
-
-                    windowManager.updateViewLayout(view,paramsTest)
-                    windowManager.updateViewLayout(view2,paramsTest2)
-                }
                 MotionEvent.ACTION_UP->{
                     view.imageview_main.apply{
                         setBackgroundResource(R.drawable.testhide)
@@ -100,6 +110,8 @@ class OverlayService: Service() {
                 }
                 else->{}
             }
+
+            mDetector.onTouchEvent(event)
             true
         }
 
@@ -130,5 +142,65 @@ class OverlayService: Service() {
 
     fun setText(showText:String){
         view2.textview_overlay.text = showText
+    }
+
+
+    inner class GestureListener:GestureDetector.SimpleOnGestureListener(){
+        private var preY:Int? =null
+        override fun onDoubleTap(e: MotionEvent?): Boolean {
+            windowManager.addView(view3,paramsTest3)
+
+            return super.onDoubleTap(e)
+        }
+
+        override fun onSingleTapConfirmed(e: MotionEvent?): Boolean {
+            view2.textview_overlay.text = strArr[Random.nextInt(5)]
+            //windowManager.removeView(view3)
+            return super.onSingleTapConfirmed(e)
+        }
+
+        override fun onScroll(
+            e1: MotionEvent?,
+            e2: MotionEvent?,
+            distanceX: Float,
+            distanceY: Float
+        ): Boolean {
+
+            if(e2!=null) {
+                if(preY==null) {
+                    preY = e2.rawY.toInt()
+                }
+                else {
+                    preY?.also {
+                        paramsTest.y += e2.rawY.toInt() - it
+                        paramsTest2.y = paramsTest.y - 200
+                    }
+                    preY = e2.rawY.toInt()
+                }
+            }
+
+
+
+
+            Log.d("dist","$distanceY")
+            view2.textview_overlay.text = ""
+            view.imageview_main.apply{
+                setBackgroundResource(R.drawable.anim_test)
+                animTest = background as AnimationDrawable
+            }
+            animTest.start()
+            windowManager.updateViewLayout(view,paramsTest)
+            windowManager.updateViewLayout(view2,paramsTest2)
+            return super.onScroll(e1, e2, distanceX, distanceY)
+        }
+
+        override fun onFling(
+            e1: MotionEvent?,
+            e2: MotionEvent?,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
+            return super.onFling(e1, e2, velocityX, velocityY)
+        }
     }
 }
